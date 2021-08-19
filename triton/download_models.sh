a59651f650ee8e7a4dbb20f517c36c2451554a5f
Emerson Jacobson <emwjacobson@gmail.com>
2021-08-18 18:57:43 -0700

    Created script to download all models

--- triton/download_models.sh
+++ triton/download_models.sh
@@ -1,16 +1,50 @@
 #!/bin/bash
 
+# Image Classification
 download_alexnet() {
-  wget 'https://github.com/onnx/models/raw/master/vision/classification/alexnet/model/bvlcalexnet-9.onnx' -O models/alexnet/1/model.onnx;
+  mkdir -p models/alexnet/1/;
+  wget -nc 'https://github.com/onnx/models/raw/master/vision/classification/alexnet/model/bvlcalexnet-9.onnx' -O models/alexnet/1/model.onnx;
 }
 
 download_mobilenet() {
-  wget 'https://github.com/onnx/models/raw/master/vision/classification/mobilenet/model/mobilenetv2-7.onnx' -O models/mobilenet/1/model.onnx;
+  mkdir -p models/mobilenet/1/;
+  wget -nc 'https://github.com/onnx/models/raw/master/vision/classification/mobilenet/model/mobilenetv2-7.onnx' -O models/mobilenet/1/model.onnx;
 }
+
+download_googlenet() {
+  mkdir -p models/googlenet/1/;
+  wget -nc 'https://github.com/onnx/models/raw/master/vision/classification/inception_and_googlenet/googlenet/model/googlenet-9.onnx' -O models/googlenet/1/model.onnx;
+}
+
+
+# Object Detection
+download_tinyyolov2() {
+  mkdir -p models/tinyyolov2/1/;
+  wget -nc 'https://github.com/onnx/models/raw/master/vision/object_detection_segmentation/tiny-yolov2/model/tinyyolov2-8.onnx' -O models/tinyyolov2/1/model.onnx;
+}
+
+download_yolov4() {
+  mkdir -p models/yolov4/1/;
+  wget -nc 'https://github.com/onnx/models/raw/master/vision/object_detection_segmentation/yolov4/model/yolov4.onnx' -O models/yolov4/1/model.onnx;
+}
+
+
+# MNIST
+download_mnist() {
+  mkdir -p models/mnist/1/;
+  wget -nc 'https://github.com/onnx/models/raw/master/vision/classification/mnist/model/mnist-8.onnx' -O models/mnist/1/model.onnx;
+}
+
 
 download_all() {
   download_alexnet;
   download_mobilenet;
+  download_googlenet;
+
+  download_tinyyolov2;
+  download_yolov4;
+
+  download_mnist;
 }
 
 download_all;
