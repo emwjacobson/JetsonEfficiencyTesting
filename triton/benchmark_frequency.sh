fb279d1ccc33ac50f74603909c764d0fc3712ab5
Emerson Jacobson <emwjacobson@gmail.com>
2021-09-28 08:36:06 -0700

    Added data for AGX

--- triton/benchmark_frequency.sh
+++ triton/benchmark_frequency.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 FILENAME="batch_X_timings.csv"
-BATCH=1
-FREQUENCIES=( 76800000 153600000 230400000 307200000 384000000 460800000 537600000 614400000 691200000 768000000 844800000 921600000 )
+BATCH=32
+FREQUENCIES=( 114750000 216750000 318750000 420750000 522750000 624750000 675750000 828750000 905250000 1032750000 1198500000 1236750000 1338750000 1377000000 )
 
 write () {
   echo $1 >> data/$FILENAME;
@@ -11,12 +11,23 @@ write () {
 write "Start,End,Frequency";
 
 # Set Max CPU Frequency at the start
-echo "1479000" | sudo tee /sys/devices/system/cpu/cpu0/cpufreq/scaling_{min,max}_freq
+# Nano:
+# echo "1479000" | sudo tee /sys/devices/system/cpu/cpu0/cpufreq/scaling_{min,max}_freq
+
+# AGX
+echo "2265600" | sudo tee /sys/devices/system/cpu/cpu0/cpufreq/scaling_{min,max}_freq
+echo "2265600" | sudo tee /sys/devices/system/cpu/cpu0/cpufreq/scaling_{min,max}_freq
 
 for freq in "${FREQUENCIES[@]}"
 do
-  echo $freq | sudo tee /sys/devices/gpu.0/devfreq/57000000.gpu/{max,min}_freq
-  echo $freq | sudo tee /sys/devices/gpu.0/devfreq/57000000.gpu/{max,min}_freq
+  # Nano
+  # echo $freq | sudo tee /sys/devices/gpu.0/devfreq/57000000.gpu/{max,min}_freq
+  # echo $freq | sudo tee /sys/devices/gpu.0/devfreq/57000000.gpu/{max,min}_freq
+
+  # AGX
+  echo $freq | sudo tee /sys/devices/17000000.gv11b/devfreq/17000000.gv11b/{max,min}_freq
+  echo $freq | sudo tee /sys/devices/17000000.gv11b/devfreq/17000000.gv11b/{max,min}_freq
+
   START=$(date +%s%3N)
   ./tritonserver/clients/bin/perf_analyzer --shared-memory=system -m mobilenet -b $BATCH -f data/$freq.csv -p 15000
   END=$(date +%s%3N)
